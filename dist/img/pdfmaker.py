d500f458d1d259ced301d4eec63be003728be0e7
pietow <pprotassow@posteo.de>
2020-01-23 17:13:05 +0100

    convert jpg to webp

---
+++ dist/img/pdfmaker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import re
+
+#file list in current directory
+files = os.listdir('.')
+#regex pattern to find jpg files
+regex = r"(.*?)\.jpg"
+
+jpg_files = []
+webp_files = []
+
+#list of jpg and webp pairs 
+file_pairs = [[re.match(regex, file).string, re.sub(regex,r"\1.webp", file )] \
+    for file in files if re.match(regex, file)]
+
+commands = ['cwebp ' + j +' -o ' + w for j, w in file_pairs]    
+
+def converte(commands):
+    for command in commands:
+        print(command)
+        os.system(command)
+
+converte(commands)
+
